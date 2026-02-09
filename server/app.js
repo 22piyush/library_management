85b2a44a9800e36b9d74680665be7730dc530444
22piyush <piyushaglawe05@gmail.com>
2026-02-09 21:43:25 +0530

    done

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}))
+}));
 
 app.use(cookieParser());
 app.use(express.json());
