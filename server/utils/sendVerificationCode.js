da4da8aa853700511dc9e92040f461c7fbca0974
22piyush <piyushaglawe05@gmail.com>
2026-02-09 23:22:27 +0530

    done

--- server/utils/sendVerificationCode.js
+++ server/utils/sendVerificationCode.js
@@ -1,3 +1,12 @@
 export async function sendVerificationCode(verificationCode, email, res) {
 
+    try {
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Verification code failed to send"
+        });
+    }
+
 }
