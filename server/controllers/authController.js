3c915538a3041c1be3349322817185f6bdc9cb40
22piyush <piyushaglawe05@gmail.com>
2026-02-09 22:33:55 +0530

    done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,13 @@ import bcrypt from "bcrypt";
 import crypto from "crypto";
 
 
-export const register = catchAyncErrors(async (req, res,) => {
+export const register = catchAyncErrors(async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return next(new ErrorHandler("Please enter all fields", 400));
+        }
+    } catch (error) {
 
+    }
 });
