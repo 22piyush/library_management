8cf1f75c1e2d41f5a68641d0d5dfd490904b9efe
22piyush <piyushaglawe05@gmail.com>
2026-02-09 21:45:56 +0530

    done

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { connectDB } from "./config/db.js"
 
 config();
 export const app = express();
@@ -15,3 +16,5 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
