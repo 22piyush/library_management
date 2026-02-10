680169486725894023313ab677d9107c6e37f6dc
22piyush <piyushaglawe05@gmail.com>
2026-02-10 22:51:32 +0530

    done

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, register, verifyOTP } from "../controllers/authController.js";
+import { getUser, login, logout, register, verifyOTP } from "../controllers/authController.js";
 import { isAuthenticate } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/register", register);
 router.post("/verify-otp", verifyOTP);
 router.post("/login", login);
 router.get("/logout",isAuthenticate, logout);
+router.get("/me",isAuthenticate, getUser);
 
 export default router;
