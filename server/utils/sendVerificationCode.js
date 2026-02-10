fbe5fd0bc215710caab72c6c3464aac7a1358fd7
22piyush <piyushaglawe05@gmail.com>
2026-02-10 21:01:57 +0530

    done

--- server/utils/sendVerificationCode.js
+++ server/utils/sendVerificationCode.js
@@ -1,4 +1,5 @@
 import { generateVerificationOtpEmailTemplate } from "./emailTemplates.js";
+import { sendEmail } from "./sendEmail.js";
 
 export async function sendVerificationCode(verificationCode, email, res) {
 
