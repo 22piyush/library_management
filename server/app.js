52375f6cab88fd8005fc5900f6f762769826294f
22piyush <piyushaglawe05@gmail.com>
2026-02-10 21:13:09 +0530

    done

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectDB } from "./config/db.js"
 import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
+import authRouter from "./routes/authRouter.js"
 
 config();
 export const app = express();
@@ -17,6 +18,8 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/auth", authRouter);
 
 connectDB();
 
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,27 +1,26 @@
 import nodemailer from "nodemailer";
 
-
-
 export const sendEmail = async (email, subject, message) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            service: process.env.SMTP_SERVICE,
+            port: process.env.SMTP_PORT,
+            auth: {
+                user: process.env.SMTP_MAIL,
+                pass: process.env.SMTP_PASSWORD,
+            },
+        });
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        service: process.env.SMTP_SERVICE,
-        port: process.env.SMTP_PORT,
-        auth: {
-            user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    });
+        const mailOptions = {
+            from: process.env.SMTP_MAIL,
+            to: email,
+            subject: subject,
+            text: message,
+        };
 
-    const mailOptions = {
-        from: process.env.SMTP_MAIL,
-        to: email,
-        subject,
-        text: message,
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.error("Email sending failed:", error);
     }
-
-
-    await transporter.sendEmail(mailOptions);
-
-}
+};
