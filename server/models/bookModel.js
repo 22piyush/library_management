4e81e27430714d72cdf03b2393ac036eb191bf7d
22piyush <piyushaglawe05@gmail.com>
2026-02-11 21:04:42 +0530

    done

---
+++ server/models/bookModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    }
+});
