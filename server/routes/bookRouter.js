0508f1ded797d51cb4b9fa33af1a1861791d2df5
22piyush <piyushaglawe05@gmail.com>
2026-02-11 21:18:26 +0530

    done

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -1,12 +1,12 @@
 import { addBook, deleteBook, getAllBook } from "../controllers/bookController.js";
-import { isAuthenticate } from "../middlewares/authMiddleware.js";
+import { isAuthenticate, isAuthorized } from "../middlewares/authMiddleware.js";
 
 
 
 const router = express.Router();
 
-router.post("/admin/add", isAuthenticate, "Authorized", addBook);
+router.post("/admin/add", isAuthenticate, isAuthorized("Admin"), addBook);
 router.get("/all", isAuthenticate, getAllBook);
-router.post("/delete/:id", isAuthenticate, "Authorized", deleteBook);
+router.post("/delete/:id", isAuthenticate, isAuthorized("Admin"), deleteBook);
 
 export default router;
