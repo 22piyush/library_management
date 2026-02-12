eb1d13fef6c4c95c8cb21c34108476f6d1c24394
22piyush <piyushaglawe05@gmail.com>
2026-02-13 00:08:26 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import OTP from "./pages/auth/OTP";
+import ResetPassword from "./pages/auth/ResetPassword";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
-    <div className='bg-gray-500'>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/otp-verification/:email" element={<OTP />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+      </Routes>
+      <ToastContainer theme="dark"/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
