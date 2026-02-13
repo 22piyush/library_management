779ed55033eda4080074298b63318816eca477c5
22piyush <piyushaglawe05@gmail.com>
2026-02-13 21:38:10 +0530

    done

---
+++ client/src/store/slices/authSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState:{
+        loading: false,
+        error: null,
+        message: null,
+        user: null,
+        isAuthenticated: false,
+    },
+    reducers:{
+        registerRequest(state){
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        },
+        registerSuccess(state, action){
+            state.loading = false;
+            state.message = action.payload.message;
+        },
+        
+    }
+})
