8796f040083d5c8222bcba45721a7bdf251c07ff
22piyush <piyushaglawe05@gmail.com>
2026-02-13 21:32:54 +0530

    done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import 'react-toastify/dist/ReactToastify.css';
-// import { Provider } from "react-redux";
-// import { store } from "./store/store.js"
+import { Provider } from "react-redux";
+import { store } from "./store/store.js"
 
 createRoot(document.getElementById("root")).render(
-    // <Provider store={store}>
+    <Provider store={store}>
         <App />
-    // </Provider>
+    </Provider>
 );
