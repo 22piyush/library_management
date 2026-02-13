df06b1bb8911ac016c5a62121be5ae736fb43966
22piyush <piyushaglawe05@gmail.com>
2026-02-13 21:44:19 +0530

    done

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -1,25 +1,43 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { act } from "react";
 
 const authSlice = createSlice({
     name: "auth",
-    initialState:{
+    initialState: {
         loading: false,
         error: null,
         message: null,
         user: null,
         isAuthenticated: false,
     },
-    reducers:{
-        registerRequest(state){
+    reducers: {
+        registerRequest(state) {
             state.loading = true;
             state.error = null;
             state.message = null;
         },
-        registerSuccess(state, action){
+        registerSuccess(state, action) {
             state.loading = false;
             state.message = action.payload.message;
         },
-        
-    }
-})
+        registerFailed(state, action) {
+            state.loading = false;
+            state.error = action.payload;
+            state.message = action.payload.message;
+        },
+    },
+});
+
+
+export const register = (data) => async(dispatch) => {
+    dispatch(authSlice.actions.registerRequest());
+    await axios.post("", data, {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then(res => {
+
+    })
+}
