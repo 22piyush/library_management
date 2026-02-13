26109fb0aaf0db2f7f99fc44b6f8dabcac7f97f7
22piyush <piyushaglawe05@gmail.com>
2026-02-13 23:21:33 +0530

    done

--- client/src/layout/Sidebar.jsx
+++ client/src/layout/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-function Sidebar() {
-  return (
-    <div></div>
-  )
+import { FaSignOutAlt as logoutIcon } from "react-icons/fa";
+import { MdDashboard as dashboardIcon } from "react-icons/md";
+import { FaBook as bookIcon } from "react-icons/fa";
+import { GiBookshelf as catalogIcon } from "react-icons/gi";
+import { MdSettings as settingIcon } from "react-icons/md";
+import { FaUsers as usersIcon } from "react-icons/fa";
+import { AiOutlineClose as closeIcon } from "react-icons/ai";
+import { BiBookContent as logo_with_title } from "react-icons/bi";
+import { RiAdminFill } from "react-icons/ri";
+
+function Sidebar({ isSidebarOpen, setIsSidebarOpen, setselectedComponent }) {
+  return <div></div>;
 }
 
-export default Sidebar
+export default Sidebar;
