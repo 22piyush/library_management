1ac0bb63499a5036b27df2e2c3f43fc2038d3c03
22piyush <piyushaglawe05@gmail.com>
2026-02-13 22:59:23 +0530

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Sidebar from "../layout/Sidebar";
 
 function Home() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -12,7 +14,29 @@ function Home() {
     return <Navigate to={"/login"} />;
   }
 
-  return <div>Home</div>;
+  return (
+    <div className="relative md:pl-64 flex min-h-screen bg-gray-100">
+      <div
+        className="md:hidden z-10 absolute right-6 top-4 sm:top-6 
+        flex justify-center items-center bg-black rounded-md h-9 w-9 text-white"
+      >
+        <GiHamburgerMenu
+          className="text-2xl"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        />
+      </div>
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+        setselectedComponent={selectedComponent}
+      />
+
+      {
+        ()
+      }
+
+    </div>
+  );
 }
 
 export default Home;
