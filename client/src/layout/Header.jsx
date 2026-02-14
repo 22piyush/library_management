f77ad12e669df8cf5466f8a4a4f806fd5c28d79c
22piyush <piyushaglawe05@gmail.com>
2026-02-14 22:21:06 +0530

    done

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -6,14 +6,16 @@ function Header() {
   const [currentTime, setCurrentTime] = useState("");
   const [currentDate, setCurrentDate] = useState("");
   useEffect(() => {
-
     const updateDateTime = () => {
-
       const now = new Date();
-      const hours = now.getHours();
+      const hours = now.getHours() % 12 || 12;
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const ampm = now.getHours() >= 12 ? "PM" : "AM";
+      setCurrentTime(`${hours}:${minutes}:${ampm}`);
 
+      const options = { month: "Short", dat: "numeric", year: "numeric" };
+      setCurrentDate(now.toLocaleDateString("en-US", options));
     };
-    
   }, []);
 
   return <div>Header</div>;
