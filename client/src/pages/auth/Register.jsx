e15890cca223b1d279cca46f9ae57bd9f2b4dbbf
22piyush <piyushaglawe05@gmail.com>
2026-02-15 22:43:02 +0530

    done

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
 
-  return (
-    <div>Register</div>
-  )
+  const { error, message, user, isAuthenticated } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate()
+
+  return <div>Register</div>;
 }
 
-export default Register
+export default Register;
