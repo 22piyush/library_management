57bd933a18daabdacff6d82aed80d5267eefd555
22piyush <piyushaglawe05@gmail.com>
2026-02-15 23:39:33 +0530

    done

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Navigate } from "react-router-dom";
+import { login } from "../../store/slices/authSlice";
+import { toast } from "react-toastify";
+import AuthLayout from "../../layout/AuthLayout";
 
 function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { error, message, isAuthenticated } = useSelector(
+    (state) => state.auth,
+  );
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+    data.append("name", name);
+    data.append("email", email);
+    data.append("password", password);
+
+    dispatch(login(data));
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="min-h-screen flex">
+      {/* LEFT SIDE IMAGE */}
+      <AuthLayout authInfo={"Create your"} />
+
+      {/* RIGHT SIDE FORM */}
+      <div className="w-full md:w-1/2 flex items-center justify-center bg-white px-6">
+        <div className="w-full max-w-md">
+          <h2 className="text-3xl font-semibold text-gray-800 mb-6 text-center">
+            Login
+          </h2>
+
+          <form onSubmit={handleLogin} className="space-y-5">
+            <div>
+              <label className="block text-sm font-medium text-gray-600 mb-1">
+                Full Name
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-600 mb-1">
+                Email Address
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-600 mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="cursor-pointer w-full bg-blue-600 text-white py-2 rounded-lg font-medium hover:bg-blue-700 transition duration-300"
+            >
+              Login
+            </button>
+          </form>
+
+          <p className="mt-6 text-center text-sm text-gray-600">
+            Dont have an account?{" "}
+            <span
+              onClick={() => navigate("/register")}
+              className="text-blue-600 font-semibold cursor-pointer hover:underline"
+            >
+              Register here!
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
