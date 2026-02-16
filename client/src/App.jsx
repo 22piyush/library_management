bb96f3cf5922cd135aa37f16e01154eebc88a46b
22piyush <piyushaglawe05@gmail.com>
2026-02-16 23:01:09 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
@@ -7,9 +7,20 @@ import ForgotPassword from "./pages/auth/ForgotPassword";
 import OTP from "./pages/auth/OTP";
 import ResetPassword from "./pages/auth/ResetPassword";
 import { ToastContainer } from "react-toastify";
-
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./store/slices/authSlice";
 
 function App() {
+  const { user, isAuthenticated, loading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  if (loading) {
+    return <span class="loader absolute top-[50%] left-[50%]"></span>;
+  }
+
   return (
     <Router>
       <Routes>
@@ -20,7 +31,7 @@ function App() {
         <Route path="/otp-verification/:email" element={<OTP />} />
         <Route path="/password/reset/:token" element={<ResetPassword />} />
       </Routes>
-      <ToastContainer theme="dark"/>
+      <ToastContainer theme="dark" />
     </Router>
   );
 }
---
+++ client/src/store/slices/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "popup",
+  initialState: {
+
+  },
+  reducers: {
+
+  },
+});
