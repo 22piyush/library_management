191eb2966db10603c727197bf0d8e613d486a558
22piyush <piyushaglawe05@gmail.com>
2026-02-18 23:08:17 +0530

    done

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,12 +3,14 @@ import authReducer from "./slices/authSlice"
 import popupReducer from "./slices/popUpSlice"
 import useReducer from "./slices/userSlice"
 import bookReducer from "./slices/bookSlice"
+import borrowReducer from "./slices/borrowSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         popup: popupReducer,
         user: useReducer,
-        book: bookReducer
+        book: bookReducer,
+        borrow: borrowReducer
     }
 })
