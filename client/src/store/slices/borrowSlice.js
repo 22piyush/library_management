ca868732b8a23157cabef26f472fbdb0eef826ff
22piyush <piyushaglawe05@gmail.com>
2026-02-18 22:40:40 +0530

    done

--- client/src/store/slices/borrowSlice.js
+++ client/src/store/slices/borrowSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { act } from "react";
 
 
 const borrowSlice = createSlice({
@@ -16,16 +15,70 @@ const borrowSlice = createSlice({
 
         fetchUserBorrowedBooksRequest(state) {
             state.loading = true;
-            state.loading = null;
+            state.error = null;
             state.message = null;
-         },
-        fetchUserBorrowedBooksSuccess(state, action) { 
+        },
+        fetchUserBorrowedBooksSuccess(state, action) {
             state.loading = false;
             state.userBorrowedBooks = action.payload;
         },
-        fetchUserBorrowedBooksFailed(state, action) { 
+        fetchUserBorrowedBooksFailed(state, action) {
             state.loading = false;
             state.error = action.payload;
+        },
+
+
+
+        recordBookRequest(state) {
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        },
+
+        recordBookSuccess(state, action) {
+            state.loading = false;
+            state.message = action.payload;
+        },
+
+        recordBookFailed(state, action) {
+            state.loading = false;
+            state.error = action.payload;
+            state.message = null;
+        },
+
+
+
+        fetchAllBorrowedBooksRequest(state) {
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        },
+        fetchAllBorrowedBooksSuccess(state, action) {
+            state.loading = false;
+            state.allBorrowedBooks = action.payload;
+        },
+        fetchAllBorrowedBooksFailed(state, action) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+
+
+
+        returnBookRequest(state) {
+            state.loading = true;
+            state.error = null;
+            state.message = null;
+        },
+
+        returnBookSuccess(state, action) {
+            state.loading = false;
+            state.message = action.payload;
+        },
+
+        returnBookFailed(state, action) {
+            state.loading = false;
+            state.error = action.payload;
+            state.message = null;
         },
 
     }
