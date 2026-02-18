741504931ebaf5e7ca90a4ca527e85ce9466a3c0
22piyush <piyushaglawe05@gmail.com>
2026-02-18 22:32:12 +0530

    done

--- client/src/store/slices/borrowSlice.js
+++ client/src/store/slices/borrowSlice.js
@@ -1,3 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+
+const borrowSlice = createSlice({
+    name: "borrow",
+    initialState: {
+        loading: false,
+        error: null,
+        userBorrowedBooks: [],
+        allBorrowedBooks: []
+    },
+    reducers: {
+
+        
+
+    }
+});
