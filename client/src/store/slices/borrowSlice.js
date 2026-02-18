d0501c7e2759cbe137020909c14cdb37d7a59459
22piyush <piyushaglawe05@gmail.com>
2026-02-18 22:35:18 +0530

    done

--- client/src/store/slices/borrowSlice.js
+++ client/src/store/slices/borrowSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { act } from "react";
 
 
 const borrowSlice = createSlice({
@@ -7,12 +8,25 @@ const borrowSlice = createSlice({
     initialState: {
         loading: false,
         error: null,
+        message: null,
         userBorrowedBooks: [],
         allBorrowedBooks: []
     },
     reducers: {
 
-        
+        fetchUserBorrowedBooksRequest(state) {
+            state.loading = true;
+            state.loading = null;
+            state.message = null;
+         },
+        fetchUserBorrowedBooksSuccess(state, action) { 
+            state.loading = false;
+            state.userBorrowedBooks = action.payload;
+        },
+        fetchUserBorrowedBooksFailed(state, action) { 
+            state.loading = false;
+            state.error = action.payload;
+        },
 
     }
 });
