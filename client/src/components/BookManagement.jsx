f674f371a7666891ffd454e91ff84380c79476f5
22piyush <piyushaglawe05@gmail.com>
2026-02-19 23:00:54 +0530

    done

--- client/src/components/BookManagement.jsx
+++ client/src/components/BookManagement.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { toggleReadBookPopup } from "../store/slices/popUpSlice";
 
 function BookManagement() {
   const dispatch = useDispatch();
 
   const { loading, error, message, books } = useSelector((state) => state.book);
   const { user, isAuthenticated } = useSelector((state) => state.auth);
-  const { addBookPopup, readBookPopup, recordBookPopup } = useSelector((state) => state.popup);
-  
+  const { addBookPopup, readBookPopup, recordBookPopup } = useSelector(
+    (state) => state.popup,
+  );
+
+  const {
+    loading: borrowSliceLoading,
+    error: borrowSliceError,
+    message: borrowSliceMessage,
+  } = useSelector((state) => state.borrow);
+
+  const [readBook, setReadBook] = useState({});
+  const openReadPopup = (id) => {
+    const book = book.find((book) => book._id === id);
+    setReadBook(book);
+    dispatch(toggleReadBookPopup());
+  };
+
+  const [borrowBookId, setBorrowBookId]
 
   return <div>BookManagement</div>;
 }
