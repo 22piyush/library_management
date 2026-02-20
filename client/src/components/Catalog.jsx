56452f956e18c90b97e5d2ec2dd69fbe11450af5
22piyush <piyushaglawe05@gmail.com>
2026-02-20 21:54:56 +0530

    done

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleAddBookPopup } from "../store/slices/popUpSlice";
 import { toast } from "react-toastify";
-import { fetchAllBooks } from "../store/slices/bookSlice";
-import { fetchAllBorrowedBook } from "../store/slices/borrowSlice";
+import { fetchAllBooks, resetBookSlice } from "../store/slices/bookSlice";
+import {
+  fetchAllBorrowedBook,
+  resetBorrowSlice,
+} from "../store/slices/borrowSlice";
 
 function Catalog() {
   const dispatch = useDispatch();
@@ -32,15 +35,21 @@ function Catalog() {
     setBorrowedBookId(bookId);
     setEmail(email);
     dispatch(toggleAddBookPopup());
-  }
+  };
 
-  useEffect(()=>{
-    if(message){
+  useEffect(() => {
+    if (message) {
       toast.success(message);
       dispatch(fetchAllBooks());
       dispatch(fetchAllBorrowedBook());
+      dispatch(resetBookSlice());
+      dispatch(resetBorrowSlice());
     }
-  })
+    if (error) {
+      toast.error(error);
+      dispatch(resetBorrowSlice());
+    }
+  });
 
   return <div>Catalog</div>;
 }
