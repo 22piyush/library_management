7c8f656a927117c5a7657b5e2912acb27c8d891c
22piyush <piyushaglawe05@gmail.com>
2026-02-20 21:53:22 +0530

    done

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { toggleAddBookPopup } from "../store/slices/popUpSlice";
+import { toast } from "react-toastify";
+import { fetchAllBooks } from "../store/slices/bookSlice";
+import { fetchAllBorrowedBook } from "../store/slices/borrowSlice";
 
 function Catalog() {
   const dispatch = useDispatch();
 
   const { returnBookPopup } = useSelector((state) => state.popup);
-  const { loading, error, message, userBorrowedBooks, allBorrowedBooks } = useSelector((state) => state.borrow);
+  const { loading, error, message, userBorrowedBooks, allBorrowedBooks } =
+    useSelector((state) => state.borrow);
 
   const currentDate = new Date();
 
-  const 
+  const borrowedBooks = allBorrowedBooks?.filter((book) => {
+    const dueDate = new Date(book.dueDate);
+    return dueDate > currentDate;
+  });
+
+  const overdueBooks = allBorrowedBooks?.filter((book) => {
+    const dueDate = new Date(book.dueDate);
+    return dueDate <= currentDate;
+  });
+
+  const bookToDisplay = filter === "borrowed" ? borrowedBooks : overdueBooks;
+
+  const [email, setEmail] = useState("");
+  const [borrowedBookId, setBorrowedBookId] = useState("");
+  const openReturnBookPopup = (bookId, email) => {
+    setBorrowedBookId(bookId);
+    setEmail(email);
+    dispatch(toggleAddBookPopup());
+  }
+
+  useEffect(()=>{
+    if(message){
+      toast.success(message);
+      dispatch(fetchAllBooks());
+      dispatch(fetchAllBorrowedBook());
+    }
+  })
 
   return <div>Catalog</div>;
 }
