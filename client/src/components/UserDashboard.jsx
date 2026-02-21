9b2bcbcdd5c603516b7459cbd02b15b16a60f7a9
22piyush <piyushaglawe05@gmail.com>
2026-02-21 22:07:40 +0530

    done

--- client/src/components/UserDashboard.jsx
+++ client/src/components/UserDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {
   Chart as ChartJS,
@@ -28,7 +28,31 @@ function UserDashboard() {
   const { settingPopup } = useSelector((state) => state.popup);
   const { userBorrowedBooks } = useSelector((state) => state.borrow);
 
-  const [] = useState(0)
+  const [totalBorrowedBooks, setTotalBorrowedBooks] = useState(0);
+  const [totalReturnedBooks, setTotalReturnedBooks] = useState(0);
+
+  useEffect(() => {
+    let numberOfTotalBorrowedBooks = userBorrowedBooks.filter(
+      (book) => book.returned === false,
+    );
+    let numberOfTotalReturnedBooks = userBorrowedBooks.filter(
+      (book) => book.returned === false,
+    );
+    setTotalBorrowedBooks(numberOfTotalBorrowedBooks.length);
+    setTotalReturnedBooks(numberOfTotalReturnedBooks.length);
+  }, [userBorrowedBooks]);
+
+
+  const data ={ 
+    labels: ["Total Borrowed Books", "Total Returned Books"],
+    datasets:[
+      {
+        data:[totalBorrowedBooks, totalReturnedBooks]
+        backgroundColor:[],
+        hoverOffset: 4
+      }
+    ]
+  }
 
   return (
     <div style={{ width: "500px" }}>
